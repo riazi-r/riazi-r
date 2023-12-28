3c02f1af246ab97ef3fc690cd99e67b992605238
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-28 16:15:28 +0330

    Update smd-extract.sh

--- vmd/smd-extract.sh
+++ vmd/smd-extract.sh
@@ -1,9 +1,6 @@
 ### Open the log file for reading and the output .dat file for writing
-global env
-set i $env(var)
-set file [open "smd-10-$i-log.txt" r]
-set file2 [open "smd.10-$i-restart-log.txt" r]
-set output [open "force$i.dat" w]
+set file [open "AmtB-SMD01A.log" r]
+set output [open "FvsP-1A.dat" w]
 
 ### Loop over all lines of the log file
 while { [gets $file line] != -1 } {
@@ -13,26 +10,11 @@ while { [gets $file line] != -1 } {
    ### by the direction of pulling (0,0,-1)
 
    if {[string range $line 0 3] == "SMD "} {
-      puts $output "[expr [lindex $line 5]]        [expr [lindex $line 6]]        [expr [lindex $line 7]]"
+      puts $output "[expr [lindex $line 4]] [expr -1*[lindex $line 7]]"
    }
 
 }
 
 ### Close the log file and the output file
 close $file
-
-while { [gets $file2 line] != -1 } {
-
-   ### Determine if a line contains SMD output. If so, write the
-   ### current z position followed by the force along z scaled by
-   ### by the direction of pulling (0,0,-1)
-
-   if {[string range $line 0 3] == "SMD "} {
-      puts $output "[expr [lindex $line 5]]        [expr [lindex $line 6]]        [expr [lindex $line 7]]"
-   }
-
-}
-
-### Close the log file and the output file
-close $file2
 close $output
