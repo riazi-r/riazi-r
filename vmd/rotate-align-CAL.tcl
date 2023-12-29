945f169464867886f4e2adb9141b0d9e61aa8171
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-30 01:31:07 +0330

    Create rotate-align-CAL.tcl

---
+++ vmd/rotate-align-CAL.tcl
@@ -0,0 +1,16 @@
+set ref [atomselect 0 "segid PROA to PROC]
+set sel [atomselect 1 "segid PROJ to PROL]
+set matrix [measure fit $sel $ref]
+set all [atomselect 1 all]
+$all move $matrix
+set cal [atomselect 1 "segid CAL"]
+$cal writepdb cal.pdb
+mol new cal.pdb
+package require psfgen	 
+topology top_all27_prot_lipid.inp	
+segment cal {pdb cal.pdb}	 
+coordpdb cal.pdb cal	 
+guesscoord	 
+writepdb cal.pdb	 
+writepsf cal.psf
+# then cal.psf and cal.pdb shall be merged with solute files to give the final solutecal files.
