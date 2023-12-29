52091d702f374a669f3b2caf71cb3f43574c0389
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-30 00:34:32 +0330

    Update dir2x.tcl

--- vmd/dir2x.tcl
+++ vmd/dir2x.tcl
@@ -1,3 +1,5 @@
+#transform molecules so a vector,e.g. $dir to be parallel to x-axis
+
 set right [atomselect top "segid PROA to PROC CARA or index 0 to 5917"]
 set left [atomselect top "segid PROJ to PROL CARB or index 5918 to 16940"]
 set com [measure center $left weight mass]
