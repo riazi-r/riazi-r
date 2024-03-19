a05cba0e56915910198006c332865dda30995da2
riazi-r <68187749+riazi-r@users.noreply.github.com>
2024-03-19 21:37:17 +0330

    Update addbond.tcl

--- vmd/addbond.tcl
+++ vmd/addbond.tcl
@@ -5,3 +5,22 @@ set b [atomselect top "segname MROF and resid 406 and name NZ"]
 set bindex [$b get index]
 
 topo addbond $aindex $bindex
+topo guessdihedrals
+topo guessangles
+#topo guessimpropers [tolerance <degrees>]
+
+#topo getbondlist [type|order|both|none]
+#topo getanglelist
+#topo get(dihedral|improper)list
+
+topo numbonds
+topo numangles
+topo numdihedrals
+topo numimpropers
+topo numcrossterms
+
+set all [atomselect top all]
+package require topotools
+set mol [::TopoTools::selections2mol $all]
+animate write psf crossed.psf $mol
+animate write pdb crossed.pdb $mol
