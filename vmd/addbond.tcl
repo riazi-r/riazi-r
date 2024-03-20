0a9d2fed132916adfa02c5f3aa00f9e667166c59
riazi-r <68187749+riazi-r@users.noreply.github.com>
2024-03-20 19:24:29 +0330

    Update addbond.tcl

--- vmd/addbond.tcl
+++ vmd/addbond.tcl
@@ -1,7 +1,7 @@
 set a [atomselect top "segname PROC and resid 398 and name CD"] 
 set aindex [$a get index]
 
-set b [atomselect top "segname MROF and resid 406 and name NZ"] 
+set b [atomselect top "segname PROL and resid 406 and name NZ"] 
 set bindex [$b get index]
 
 topo addbond $aindex $bindex
